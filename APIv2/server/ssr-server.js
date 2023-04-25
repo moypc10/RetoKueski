e879f8d011bf0769d7939300080f73193464cf01
esquivelgor <esquivelguillerm@gmail.com>
2023-04-24 23:45:00 -0600

    V 2.0.1
    Input algunos endpoints

--- APIv2/server/ssr-server.js
+++ APIv2/server/ssr-server.js
@@ -6,19 +6,69 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare()
-.then(() => {
-  const server = express()
+  .then(() => {
+    const server = express()
 
-  server.get('*', (req, res) => {
-    return handle(req, res)
-  })
+    server.get('/main', (req, res) => {
+      return handle(req, res)
+    })
 
-  server.listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    server.get("/test", (req, res) => {
+      console.log("testtt")
+      res.json({ message: "Hello from server side!" });
+    })
+
+    server.get("/petitions/pending", async (req, res) => {
+      res.json({ message: "petitions/pending!" });
+      //try {
+      //  const connection = await pool.getConnection();
+      //  const [rows] = await connection.execute("CALL sp_get_pending_petitions() ");
+      //  connection.release();
+      //  res.json(rows);
+      //}
+      //catch (err) {
+      //  console.error(err);
+      //  res.status(500).send('Error retrieving data from the database');
+      //}
+    });
+
+    server.get("/petitions/notPending", async (req, res) => {
+      res.json({ message: "petitions/notPending!" });
+      //try {
+      //  const connection = await pool.getConnection();
+      //  const [rows] = await connection.execute("CALL sp_get_not_pending_petitions() ");
+      //  connection.release();
+      //  res.json(rows);
+      //}
+      //catch (err) {
+      //  console.error(err);
+      //  res.status(500).send('Error retrieving data from the database');
+      //}
+    })
+
+    server.get('/petitions/user/:id', async (req, res) => {
+      res.json({ message: "petitions/user/id" });
+      //const { id } = req.params;
+      //
+      //try {
+      //  const connection = await pool.getConnection();
+      //  const [rows] = await connection.execute('CALL get_client_info(?)', [id]);
+      //  connection.release();
+      //  res.json(rows);
+      //
+      //} catch (err) {
+      //  console.error(err);
+      //  res.status(500).send('Error retrieving data from the database');
+      //}
+    });
+
+
+    server.listen(3000, (err) => {
+      if (err) throw err
+      console.log('> Ready on http://localhost:3000')
+    })
   })
-})
-.catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
-})
+  .catch((ex) => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
